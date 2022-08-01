5fe739f90a32634850e4fd03fc4876daea8b872a
FBernardino <filipebernardinofaria@gmail.com>
2022-08-01 09:15:06 -0300

    Implementado Métodos de Subtração, Multiplicação, Divisão, Média e Raiz Quadrada

--- 01_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ 01_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -19,7 +19,7 @@ namespace RestWithASPNETUdemy.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
@@ -29,6 +29,81 @@ namespace RestWithASPNETUdemy.Controllers
 
             return BadRequest("Invalid Input !");
         }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid Input !");
+        }
+
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+            return BadRequest("Invalid Input !");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(firstNumber) == 0 || ConvertToDecimal(secondNumber) == 0) 
+                {
+                    return BadRequest("Cannot divide by zero");
+                }
+
+                var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(div.ToString());
+            }
+            return BadRequest("Invalid Input !");
+        }
+
+        [HttpGet("aver/{firstNumber}/{secondNumber}/{thirdNumber}")]
+        public IActionResult Aver(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber) && IsNumeric(thirdNumber))
+            {
+                var aver = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber) + ConvertToDecimal(thirdNumber)) / 3 ;
+                return Ok(aver.ToString());
+            }
+            return BadRequest("Invalid Input !");
+        }
+
+        [HttpGet("root/{firstNumber}")]
+        public IActionResult Root(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var root = Math.Sqrt(ConvertToDouble(firstNumber));
+                return Ok(root.ToString());
+            }
+            return BadRequest("Invalid Input !");
+        }
+
+
+        private double ConvertToDouble(string strNumber)
+        {
+            double doubleValue;
+            if(double.TryParse(strNumber, out doubleValue))
+            {
+                return doubleValue;
+            }
+
+            return 0;
+
+        }
+
         private bool IsNumeric(string strNumber)
         {
             double number;
